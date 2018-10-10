9134ca4ec0464089482b9e01a11988b3855a47b5
lc2958@columbia.edu <lc2958@columbia.edu>
2018-10-09 21:33:40 -0400

    added procfile for heroku config

--- app.py
+++ app.py
@@ -11,5 +11,15 @@ def receive_message():
 	if request.method == 'GET':
 		token_sent = request.args.get("hub.verify_token")
 
+
+def verify_fb_token(token_sent):
+    """ token_sent: sent by facebook 
+    	description: 
+    	verifies if it matches the verify token, or returns error """
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+
 if __name__ == '__main__':
 	app.run()
