675e4e12a79229fa98a7c642b212c7c83fcb9f18
lc2958@columbia.edu <lc2958@columbia.edu>
2018-10-09 21:08:33 -0400

    added verify token

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from pymessenger.bot import Bot
 import os
 
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def receive_message():
-	return "Hello World"
+	if request.method == 'GET':
+		token_sent = request.args.get("hub.verify_token")
 
 if __name__ == '__main__':
 	app.run()
