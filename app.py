97da84ea1c87c5f928429b6061e2062bb2554d5b
Lesley Cordero <lesleyc@lesleyc-macbookpro.roam.corp.google.com>
2018-10-16 20:50:36 -0400

    revert back

--- app.py
+++ app.py
@@ -37,9 +37,8 @@ def verify_fb_token(token_sent):
 	 	verifies if it matches the verify token, or returns error """
 	 if token_sent == VERIFY_TOKEN:
 	 	return request.args.get("hub.challenge")
-	 #return 'Invalid verification token:' + str(token_sent) + str(VERIFY_TOKEN)
-	 return request.args.get("hub.challenge")
-	 
+	 return 'Invalid verification token:' + str(token_sent) + str(VERIFY_TOKEN)
+
 def get_message():
 	sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
 	return random.choice(sample_responses)
@@ -49,4 +48,4 @@ def send_message(recipient_id, response):
 	return "success"
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
