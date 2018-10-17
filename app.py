9642e091ad7f14f67e0330e07dc664be1aaedf77
Lesley Cordero <lesleyc@lesleyc-macbookpro.roam.corp.google.com>
2018-10-16 20:27:25 -0400

    testing verify token

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ bot = Bot (ACCESS_TOKEN)
 @app.route('/', methods=['GET', 'POST'])
 def receive_message():
 	if request.method == 'GET':
-		token_sent = request.args.get("hub.verify_token", '')
+		token_sent = request.args.get("hub.verify_token")
 		return verify_fb_token(token_sent)
 	else:
 		output = request.get_json()
@@ -37,8 +37,9 @@ def verify_fb_token(token_sent):
 	 	verifies if it matches the verify token, or returns error """
 	 if token_sent == VERIFY_TOKEN:
 	 	return request.args.get("hub.challenge")
-	 return 'Invalid verification token:' + str(token_sent) + str(VERIFY_TOKEN)
-
+	 #return 'Invalid verification token:' + str(token_sent) + str(VERIFY_TOKEN)
+	 return request.args.get("hub.challenge")
+	 
 def get_message():
 	sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
 	return random.choice(sample_responses)
