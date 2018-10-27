08b0664ec384a34ca8c66d2c547200f10be583c3
lc2958@columbia.edu <lc2958@columbia.edu>
2018-10-26 21:07:19 -0400

    added some code outlinee

--- app.py
+++ app.py
@@ -3,14 +3,38 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+
+def exist_user(phone_number):
+	# DB call here
+	return ""
+
+def new_user(phone_number):
+	# insert into db 
+	# CODE HERE
+	resp = MessagingResponse()
+	resp.message("Welcome to Skybot! What's your UNI?")
+
+	return str(resp)
+
+def check_verification(phone_number):
+
+	return ""
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
 
+	# gets phone number of user
 	body = request.values.get('From', None)
+	
+	# checks db for existing user
+	# CODE HERE
+
+	# depending on result, call exist or new user function
 
 	resp = MessagingResponse()
 	
 	resp.message("Welcome to Skybot")
+	
 	return str(resp)
 
 
