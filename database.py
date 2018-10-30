a9f0031ddc180d56601baf8ee1621954bcc1c874
Joi <37125335+joicodes@users.noreply.github.com>
2018-10-30 14:29:46 -0400

    unused importe

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security  import generate_password_hash, check_password_hash
 import sqlite3
 import datetime
 
