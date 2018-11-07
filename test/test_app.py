6064cdac797fdbfcf3e12c9823a310e8dacd6432
Maria Javier <mariabjavier@gmail.com>
2018-11-07 12:44:36 -0500

    first test testing new user

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,21 @@
 import unittest
+import requests
+import random
+import xml.etree.ElementTree as ET
+from twilio.rest import Client
 
-# do we need an extra phone number for testing purposes?
+
 class AppTests(unittest.TestCase):
-	def test_exist_user(self):
-		return ""
+	def test_new_user(self):            
+		req = requests.post('https://cuskybot.herokuapp.com/sms', data = {'To': '+16674014282', 'From': '+13476537652', 'Body': 'me'})
+		root = ET.fromstring(str(req.text))
+		for child in root:
+			message = child.text
+			break
+		
+		self.assertEqual(message, "Welcome to Skybot! What's your UNI?")
+
+
+if __name__ == '__main__':
+	unittest.main()
+
