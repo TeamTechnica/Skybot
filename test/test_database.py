bba038db5efeafa5cc34032fe66aba96ef791c24
Maria Javier <mariabjavier@gmail.com>
2018-11-24 11:35:48 -0500

    changed directory structure/tests passing

--- test/test_database.py
+++ test/test_database.py
@@ -1,5 +1,5 @@
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
+import sys
+sys.path.append('Skybot/')
 from databaseChanges import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
