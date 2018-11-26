822b4c445da39e83629ce731d19e583de1d43dbb
Klarizsa Padilla <ksp35@georgetown.edu>
2018-11-26 10:25:13 -0500

    changed the engine creation and table fields

--- databaseChanges.py
+++ databaseChanges.py
@@ -10,10 +10,11 @@ from sqlalchemy.orm import sessionmaker, relationship
 import sys
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #db = SQLAlchemy(app)
+engine = sqlalchemy.create_engine('sqlite:///:memory:')
 Base = declarative_base()
 
 
@@ -32,8 +33,8 @@ class User(Base):
     uni = Column(String(80), nullable=False, unique=True)
     max_passengers = Column(Integer)
     phone_number = Column(String(50), unique = True)
-    verification_code = Column(Integer)
-    verified = Column(String(10), default='NONE')
+    #verification_code = Column(Integer)
+    #verified = Column(String(10), default='NONE')
 
     stock = relationship('Match', backref='user',
                          primaryjoin=id == Match.user_id)
@@ -43,7 +44,7 @@ class Flight(Base):
     __tablename__ = 'flights'
     id = Column(Integer, primary_key=True)
     #creation_date = Column(datetime(timezone=True), default=datetime.datetime.utcnow)
-    flight_num = Column(String(6))
+    #flight_num = Column(String(6))
     airport = Column(String(3))
     flight_date =  Column(Integer)
     departure_time = Column(Integer)
