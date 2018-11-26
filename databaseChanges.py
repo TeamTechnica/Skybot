4cc437d94273c1129189b9b272c432ffb7e7a812
Klarizsa Padilla <ksp35@georgetown.edu>
2018-11-26 10:34:02 -0500

    updated db to working state

--- databaseChanges.py
+++ databaseChanges.py
@@ -53,5 +53,33 @@ class Flight(Base):
     stock = relationship('Match', backref='flight',
                          primaryjoin=id == Match.flight_id)
 
-if __name__ == '__database__':
-    app.run(Debug=True)
+
+# Base.metadata.create_all(engine)
+
+# Session = sessionmaker(bind=engine)
+# session = Session()
+
+
+# test_user1 = User(id=1, uni='test100', max_passengers = 2, phone_number = "100000001")
+# test_user2 = User(id=2, uni='test101', max_passengers = 1, phone_number = "100000002")
+# test_user3 = User(id=3, uni='test102', max_passengers = 2, phone_number = "100000003")
+# test_user4 = User(id=4, uni='test103', max_passengers = 2, phone_number = "100000004")
+# print ("hello")
+
+# test_flight1 = Flight(id=0, airport = 'JFK', flight_date = 10312018, departure_time = 122800)
+# test_flight2 = Flight(id= 1, airport = 'JFK', flight_date = 10312018, departure_time = 123000)
+# test_flight3 = Flight(id=2, airport = 'LGA', flight_date = 12252018, departure_time = 124500)
+# test_flight4 = Flight(id=3, airport = 'LGA', flight_date = 12252018, departure_time = 100000)
+# test_flight5 = Flight(id=4, airport = 'LGA', flight_date = 12252018, departure_time = 100000)
+
+# session.add(test_user1)
+# session.add(test_user2)
+# session.add(test_user3)
+# session.add(test_user4)
+# session.add(test_flight1)
+# session.add(test_flight2)
+# session.add(test_flight3)
+# session.add(test_flight4)
+# session.add(test_flight5)
+
+# session.commit()
