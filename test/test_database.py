f1a67cea0c02805664031608bb2b649f057182f2
Klarizsa Padilla <ksp35@georgetown.edu>
2018-11-27 21:28:16 -0500

    Update test_database.py
    resolved syntax issues

--- test/test_database.py
+++ test/test_database.py
@@ -18,7 +18,6 @@ db.drop_all()
 db.create_all()
 
 
-
 # Creates test database instances 
 
 test_user1 = User(uni='test100', max_passengers = 2, phone_number = "100000001")
@@ -40,30 +39,28 @@ test_flight5 = Flight(airport = 'LGA', flight_date = "12252018", departure_time 
 class TestDatabase(unittest.TestCase):
 
     # Testing for schema
-
-    def test_UserSchema(self):
+    
+    def test_UserSchema(self):    
         result = str(User)
         self.assertEqual(result, "<class 'databaseChanges.User'>")
 
-    def test_FlightSchema(self):
+    def test_FlightSchema(self):    
         result = str(Flight)
         self.assertEqual(result, "<class 'databaseChanges.Flight'>")
 
-    def test_MatchSchema(self):
+    def test_MatchSchema(self):    
         result = str(Match)
         self.assertEqual(result, "<class 'databaseChanges.Match'>")
 
-    # Test querying data
-
+    #  Test querying data
+    
     def test_AddingUsers(self):
-
         db.session.add(test_user1)
         db.session.add(test_user2)
         db.session.add(test_user3)
         db.session.add(test_user4)
         db.session.commit()
     
-
         result = str(User.query.all())
         self.assertEqual(result, "[<User 1>, <User 2>, <User 3>, <User 4>]")
 
@@ -78,14 +75,14 @@ class TestDatabase(unittest.TestCase):
         result = str(Flight.query.all())
         self.assertEqual(result, "[<Flight 1>, <Flight 2>, <Flight 3>, <Flight 4>, <Flight 5>]")
 
-    # Checks whether existing user's phone number can be found in the db
+    # Checks whether an given existing user's phone number can be found in the database
     def test_UserQuery(self):
 
         pnumber = '100000001'
         check_num = db.session.query(User).filter(User.phone_number == pnumber)
         result = db.session.query(check_num.exists()).scalar()
         self.assertEqual(result, True)
-
+    
     # Query a user's max passengers and phone number based on uni
     def test_ReturnUserInfo(self):
         test_uni = 'test101'
@@ -104,9 +101,8 @@ class TestDatabase(unittest.TestCase):
         flight_info = []
 
         for i in range (0,len(matching_flights)):
-            flight_info.append((matching_flights[i].airport,
-                                matching_flights[i].departure_time ))
-
+            flight_info.append((matching_flights[i].airport, matching_flights[i].departure_time ))
+       
         result = flight_info
         self.assertEqual(result, [('JFK', 123000), ('JFK', 123000)])
 
@@ -115,20 +111,15 @@ class TestDatabase(unittest.TestCase):
     def test_Delete(self):
         Flight.query.filter_by(id=test_flight3.id).delete()
         results = str(Flight.query.all())
-        self.assertEqual(results, "[<Flight 1>, <Flight 2>,
-                            <Flight 4>, <Flight 5>]")
+        self.assertEqual(results, "[<Flight 1>, <Flight 2>, <Flight 4>, <Flight 5>]")
 
     def test_RepeatedUni(self):
         error = "false"
         try:
-<
             test_user6 = User(uni='test100', max_passengers = 2, phone_number = "100000001")
             db.session.add(test_user6)
             db.session.commit()
-
         except:
             db.session.rollback()
             error = "User is already in database"
-
         self.assertEqual(error, "User is already in database")
-
