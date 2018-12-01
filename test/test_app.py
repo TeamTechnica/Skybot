7cdf19aafdb42d6b5921829d50207bdd13a62893
Maria Javier <mariabjavier@gmail.com>
2018-12-01 14:22:55 -0500

    pausing tests for now

--- test/test_app.py
+++ test/test_app.py
@@ -16,134 +16,105 @@ phone_num1 = '+' + str(random.randint(10000000000, 11111111111))
 phone_num2 = '+' + str(random.randint(10000000000, 11111111111))
 
 
-class AppTests(unittest.TestCase):
+req = requests.post(
+    'https://cuskybot.herokuapp.com/sms',
+    data={
+        'To': '+16674014282',
+        'From': phone_num1, 'Body': 'Hello There!',
+    },
+)
+root = ET.fromstring(str(req.text))
+for child in root:
+    message = child.text
+    break
 
-    def test_new_user1(self):
-        req = requests.post(
-            'https://cuskybot.herokuapp.com/sms',
-            data={
-                'To': '+16674014282',
-                'From': phone_num1, 'Body': 'Hello There!',
-            },
-        )
-        root = ET.fromstring(str(req.text))
-        for child in root:
-            message = child.text
-            break
+if message != "Welcome to Skybot! What's your UNI?":
+    raise ValueError('Unexpected Behavior. Bot give no intro')
 
-        self.assertEqual(message, "Welcome to Skybot! What's your UNI?")
 
-    def test_new_user1(self):
-        req = requests.post(
-            'https://cuskybot.herokuapp.com/sms',
-            data={
-                'To': '+16674014282',
-                'From': phone_num2, 'Body': 'Hello There!',
-            },
-        )
-        root = ET.fromstring(str(req.text))
-        for child in root:
-            message = child.text
-            break
+req = requests.post(
+    'https://cuskybot.herokuapp.com/sms',
+    data={
+        'To': '+16674014282',
+        'From': phone_num2, 'Body': 'Hello There!',
+    },
+)
+root = ET.fromstring(str(req.text))
+for child in root:
+    message = child.text
+    break
 
-        self.assertEqual(message, "Welcome to Skybot! What's your UNI?")
+if message != "Welcome to Skybot! What's your UNI?":
+    raise ValueError('Unexpected Behavior. Bot give no intro')
 
-    def test_user1_wrong_uni(self):
-        req = requests.post(
-            'https://cuskybot.herokuapp.com/sms',
-            data={
-                'To': '+16674014282',
-                'From': phone_num1, 'Body': 'mj345',
-            },
-        )
 
-        root = ET.fromstring(str(req.text))
-        for child in root:
-            message = child.text
-            break
+req = requests.post(
+    'https://cuskybot.herokuapp.com/sms',
+    data={
+        'To': '+16674014282',
+        'From': phone_num1, 'Body': 'mj345',
+    },
+)
 
-        self.assertEqual(message, """Error: Invalid uni!""")
+root = ET.fromstring(str(req.text))
+for child in root:
+    message = child.text
+    break
 
-        # send correct uni now
-        req = requests.post(
-            'https://cuskybot.herokuapp.com/sms',
-            data={
-                'To': '+16674014282',
-                'From': phone_num1, 'Body': 'mj3456',
-            },
-        )
+if message != "Error: Invalid uni!":
+    raise ValueError(
+        'Unexpected Behavior. Bot does not check for invalid unis',
+    )
 
-        root = ET.fromstring(str(req.text))
-        for child in root:
-            message = child.text
-            break
+# send correct uni now
+req = requests.post(
+    'https://cuskybot.herokuapp.com/sms',
+    data={
+        'To': '+16674014282',
+        'From': phone_num1, 'Body': 'mj3456',
+    },
+)
 
-        self.assertEqual(
-            message, """Check your email for a verification email
-            and text us the code""",
-        )
+root = ET.fromstring(str(req.text))
+for child in root:
+    message = child.text
+    break
 
-    def test_new_user1(self):
-        req = requests.post(
-            'https://cuskybot.herokuapp.com/sms',
-            data={
-                'To': '+16674014282',
-                'From': phone_num2, 'Body': 'mj6789',
-            },
-        )
-        root = ET.fromstring(str(req.text))
-        for child in root:
-            message = child.text
-            break
+if message != """Check your email for a verification email
+    and text us the code""":
+    print(message)
+    raise ValueError(
+        'Unexpected Behavior. Bot does not check for email verification',
+    )
 
-        self.assertEqual(
-            message, """Check your email for a verification email
-            and text us the code""",
-        )
 
-    '''
-    def test_uni_login(self):
-        req = requests.post('https://cuskybot.herokuapp.com/sms',
-                            data = {'To': '+16674014282',
-                            'From': rand_num, 'Body': 'mj2729'})
-        root = ET.fromstring(str(req.text))
-        for child in root;
-            message = child.text
-            break
+req = requests.post(
+    'https://cuskybot.herokuapp.com/sms',
+    data={
+        'To': '+16674014282',
+        'From': phone_num2, 'Body': 'mj6789',
+    },
+)
+root = ET.fromstring(str(req.text))
+for child in root:
+    message = child.text
+    break
 
-        self.assertEqual(message, "Check your email for a verification email
-            and text us the code")
+if message != """Check your email for a verification email
+    and text us the code""":
+    raise ValueError(
+        'Unexpected Behavior. Bot does not check for email verification',
+    )
 
-    def test_verfication(self)
-        user = session.query(Users).filter(User.uni == mj2729).first()
-        user_verification = user.verification_code
+user1 = User.query.filter_by(phone_number=phone_num1).first()
+if user1 is not None:
+    db.session.delete(user1)
+    db.session.commit()
 
-        req = requests.post('https://cuskybot.herokuapp.com/sms',
-                            data = {'To': '+16674014282',
-                            'From': rand_num, 'Body': user_verification})
-
-        root = ET.fromstring(str(req.text))
-        for child in root;
-            message = child.text
-            break
-
-        self.assertEqual(message, str(Thanks for verifying! Let's start
-        flight information. Please answer the following, separated by commas:
-        1. JFK/LGA/EWR
-        2. Date (MM/DD/YYYY)
-        3. Flight Time (XX:XX AM/PM)
-        4. Maximum Number of Additional Passengers))
-    '''
-
-    user1 = User.query.filter_by(phone_number=phone_num1).first()
-    if user1 is not None:
-        db.session.delete(user1)
-        db.session.commit()
-
-    user2 = User.query.filter_by(phone_number=phone_num2).first()
-    if user1 is not None:
-        db.session.delete(user1)
-        db.session.commit()
+user2 = User.query.filter_by(phone_number=phone_num2).first()
+if user1 is not None:
+    db.session.delete(user1)
+    db.session.commit()
 
 
 if __name__ == '__main__':
