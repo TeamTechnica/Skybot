af910aa946a9db0c26b9e5326c90cfeb1fc31dc2
Maria Javier <mariabjavier@gmail.com>
2018-12-02 02:08:14 -0500

    again trying to fix precommit changes

--- test/test_app.py
+++ test/test_app.py
@@ -1,13 +1,16 @@
 import os
-import sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-import unittest
-import requests
 import random
+import sys
+import unittest
+import xml.etree.ElementTree as ET
+
+import requests
+from twilio.rest import Client
+
 from app import *
 from database import *
-import xml.etree.ElementTree as ET
-from twilio.rest import Client
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 verfi_num1 = str(random.randint(100000, 111111))
 verfi_num2 = str(random.randint(100000, 111111))
@@ -16,105 +19,33 @@ phone_num1 = '+' + str(random.randint(10000000000, 11111111111))
 phone_num2 = '+' + str(random.randint(10000000000, 11111111111))
 
 
-req = requests.post(
-    'https://cuskybot.herokuapp.com/sms',
-    data={
-        'To': '+16674014282',
-        'From': phone_num1, 'Body': 'Hello There!',
-    },
-)
-root = ET.fromstring(str(req.text))
-for child in root:
-    message = child.text
-    break
+class Test(unittest.TestCase):
+    def test_new_user1(self):
+        self.test_app = app.test_client()
 
-if message != "Welcome to Skybot! What's your UNI?":
-    raise ValueError('Unexpected Behavior. Bot give no intro')
+        response = self.test_app.post(
+            '/sms', data={
+                'To': '+16674014282',
+                'From': phone_num1, 'Body': 'Hello There!',
+            },
+        )
 
+        root = ET.fromstring(str(response.data))
+        for child in root:
+            message = child.text
+            break
 
-req = requests.post(
-    'https://cuskybot.herokuapp.com/sms',
-    data={
-        'To': '+16674014282',
-        'From': phone_num2, 'Body': 'Hello There!',
-    },
-)
-root = ET.fromstring(str(req.text))
-for child in root:
-    message = child.text
-    break
+        self.assertEqual(message, "Welcome to Skybot! What's your UNI?")
 
-if message != "Welcome to Skybot! What's your UNI?":
-    raise ValueError('Unexpected Behavior. Bot give no intro')
+    user1 = User.query.filter_by(phone_number=phone_num1).first()
+    if user1 is not None:
+        db.session.delete(user1)
+        db.session.commit()
 
-
-req = requests.post(
-    'https://cuskybot.herokuapp.com/sms',
-    data={
-        'To': '+16674014282',
-        'From': phone_num1, 'Body': 'mj345',
-    },
-)
-
-root = ET.fromstring(str(req.text))
-for child in root:
-    message = child.text
-    break
-
-if message != "Error: Invalid uni!":
-    raise ValueError(
-        'Unexpected Behavior. Bot does not check for invalid unis',
-    )
-
-# send correct uni now
-req = requests.post(
-    'https://cuskybot.herokuapp.com/sms',
-    data={
-        'To': '+16674014282',
-        'From': phone_num1, 'Body': 'mj3456',
-    },
-)
-
-root = ET.fromstring(str(req.text))
-for child in root:
-    message = child.text
-    break
-
-if message != """Check your email for a verification email
-    and text us the code""":
-    print(message)
-    raise ValueError(
-        'Unexpected Behavior. Bot does not check for email verification',
-    )
-
-
-req = requests.post(
-    'https://cuskybot.herokuapp.com/sms',
-    data={
-        'To': '+16674014282',
-        'From': phone_num2, 'Body': 'mj6789',
-    },
-)
-root = ET.fromstring(str(req.text))
-for child in root:
-    message = child.text
-    break
-
-if message != """Check your email for a verification email
-    and text us the code""":
-    raise ValueError(
-        'Unexpected Behavior. Bot does not check for email verification',
-    )
-
-user1 = User.query.filter_by(phone_number=phone_num1).first()
-if user1 is not None:
-    db.session.delete(user1)
-    db.session.commit()
-
-user2 = User.query.filter_by(phone_number=phone_num2).first()
-if user1 is not None:
-    db.session.delete(user1)
-    db.session.commit()
+    user2 = User.query.filter_by(phone_number=phone_num2).first()
+    if user1 is not None:
+        db.session.delete(user1)
+        db.session.commit()
 
 
 if __name__ == '__main__':
