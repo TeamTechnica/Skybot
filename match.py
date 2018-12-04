51578e747288812b720859ec2a6046e600fe9a5b
Joi <37125335+joicodes@users.noreply.github.com>
2018-12-03 23:51:59 -0500

    Update match.py

--- match.py
+++ match.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import exists
 from sqlalchemy.orm import sessionmaker
 
-from app import *
+from app import db, User, Flight, Match
 
 '''
 engine = create_engine(
