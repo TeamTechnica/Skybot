0bc181ab8bdecfad17b1a2c2f96db4c7817f3681
Lesley <lc2958@columbia.edu>
2018-12-18 09:28:39 -0500

    cleaned up cost method -- no need to make so many get requests

--- app.py
+++ app.py
@@ -44,30 +44,22 @@ def get_cost(location, num_passengers):
     Return:
         cost (str): cost of the lyft ride
     """
-    # Gets the cost of a Lyft from Columbia to JFK Airport
-    jfk = requests.get('https://api.lyft.com/v1/cost?start_lat=40.8075&start_lng=-73.9626&end_lat=40.6413&end_lng=-73.7781')
-    jfk_json = json.loads(jfk.text)
-
-    # Gets the cost of a Lyft from Columbia to La Guardia Airport (LGA)
-    lga = requests.get('https://api.lyft.com/v1/cost?start_lat=40.8075&start_lng=-73.9626&end_lat=40.7769&end_lng=-73.8740')
-    lga_json = json.loads(lga.text)
-
-    # Gets cost of a Lyft from Columbia to Newark International Airport (EWR)
-    ewr = requests.get('https://api.lyft.com/v1/cost?start_lat=40.8075&start_lng=-73.9626&end_lat=40.6895&end_lng=-74.1745')
-    ewr_json = json.loads(ewr.text)
-
     # Returns the cost of the ride based on the passed airport location
     if location.upper() == "JFK":
-        jfk_cost = '%.2f' % ((jfk_json['cost_estimates'][1]['estimated_cost_cents_max']/100) / num_passengers)
-        cost = '$'+str(jfk_cost)
-    if location.upper() == "LGA":
-        lga_cost = '%.2f' % ((lga_json['cost_estimates'][1]['estimated_cost_cents_max']/100) / num_passengers)
-        cost = '$'+str(lga_cost)
-    if location.upper() == "EWR":
-        ewr_cost = '%.2f' % ((ewr_json['cost_estimates'][1]['estimated_cost_cents_max']/100) / num_passengers)
-        cost = '$'+str(ewr_cost)
-
-    return cost
+        jfk = requests.get('https://api.lyft.com/v1/cost?start_lat=40.8075&start_lng=-73.9626&end_lat=40.6413&end_lng=-73.7781')
+        jfk_cost = '%.2f' % ((json.loads(jfk.text)['cost_estimates'][1]['estimated_cost_cents_max']/100) / num_passengers)
+        return '$'+str(jfk_cost)
+    elif location.upper() == "LGA":
+        lga = requests.get('https://api.lyft.com/v1/cost?start_lat=40.8075&start_lng=-73.9626&end_lat=40.7769&end_lng=-73.8740')
+        lga_json = json.loads(lga.text)
+        lga_cost = '%.2f' % ((json.loads(lga.text)['cost_estimates'][1]['estimated_cost_cents_max']/100) / num_passengers)
+        return '$'+str(lga_cost)
+    elif location.upper() == "EWR":
+        ewr = requests.get('https://api.lyft.com/v1/cost?start_lat=40.8075&start_lng=-73.9626&end_lat=40.6895&end_lng=-74.1745')
+        ewr_cost = '%.2f' % ((json.loads(ewr.text)['cost_estimates'][1]['estimated_cost_cents_max']/100) / num_passengers)
+        return '$'+str(ewr_cost)
+    else:
+        return "Cost could not be calculated"
 
 
 def notify_user(phone_number, unis, cost):
